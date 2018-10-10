fba0657c06ddcb96ffadd4059c8d3434cea93d38
Srinivasan <sundarsv@gmail.com>
2018-10-10 06:38:09 +0530

    Styling header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Header.css';
 import Button from '@material-ui/core/Button';
 
 class Header extends Component {
     render () {
         return (
-            <div>
-                <Button variant="contained" color="default">Login</Button>
+            <div className="headerContainer">
+                <Button variant="contained" className="loginButton" color="default">Login</Button>
             </div>
         )
     }
