a6096c8da6c058e0d4ecc8945d5fd7dfa6229e16
Srinivasan <sundarsv@gmail.com>
2018-10-12 06:30:11 +0530

    Code alignment with course and adding 'Logo' to the 'Header'

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
 
 class Header extends Component {
     render () {
         return (
-            <div className="headerContainer">
-                <Button variant="contained" className="loginButton" color="default">Login</Button>
+            <div>
+                <header className="app-header">
+                <img src = {logo} className="app-logo" alt="logo"/>
+                    <div className="login-button">
+                        <Button variant="contained" color="default">Login</Button>
+                    </div>
+                </header>
             </div>
         )
     }
