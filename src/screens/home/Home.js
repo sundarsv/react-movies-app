2b0f4c260bf50cbba7fc0d999b133818a8f7aa22
Srinivasan <sundarsv@gmail.com>
2018-10-13 07:58:44 +0530

    Added username and password inside Login form

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Home.css';
-import Header from '../../common/header/Header'
+import Header from '../../common/header/Header';
+import Typography from '@material-ui/core/Typography';
 
 class Home extends Component {
     render() {
