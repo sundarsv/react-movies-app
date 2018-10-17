458fa79e40b0a0305e37c989261546c1df2b0af2
Srinivasan <sundarsv@gmail.com>
2018-10-17 21:59:42 +0530

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import moviesData from '../../assets/movieData';
 import './Details.css';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import Home from '../../screens/home/Home';
+import YouTube from 'react-youtube';
 
 class Details extends Component {
     constructor() {
@@ -20,12 +23,28 @@ class Details extends Component {
         this.setState({currentState});
         console.log(this.state);
     }
+
+    backtohomeHandler = () => {
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
     
     render() {
         let movie = this.state.movie;
+        const opts = {
+            height: '300',
+            width: '700',
+            playerVars: {
+                autoplay: 1
+            }
+        }
         return(
             <div className="details">
                 <Header />
+                <div className="back">
+                    <Typography onClick={this.backtohomeHandler}>
+                    &#60; Back to Home
+                    </Typography>
+                </div>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
                         <img src={movie.poster_url} alt={movie.title}/>
@@ -66,6 +85,14 @@ class Details extends Component {
                                 <span className="bold">Plot: </span>
                                 <span><a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline}</span>
                             </Typography>
+                        </div>
+                        <div className="trailerContainer">
+                            <Typography><span className="bold">Trailer: </span></Typography>
+                            <YouTube 
+                                videoId={movie.trailer_url.split("?v=")[1]}
+                                opts={opts}
+                                onReady={this._onReady}
+                            />
                         </div>
                     </div>
                     <div className="rightDetails">
